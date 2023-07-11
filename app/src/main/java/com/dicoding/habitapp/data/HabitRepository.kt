2b1d7d0450c09ee6cefcba1c58a4a17ba15d5516
Ayu Erawan <ayu13pradnyandari@gmail.com>
2023-07-11 13:41:51 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/habitapp/data/HabitRepository.kt
@@ -0,0 +1,67 @@
+package com.dicoding.habitapp.data
+
+import android.content.Context
+import android.nfc.tech.MifareUltralight.PAGE_SIZE
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.dicoding.habitapp.utils.HabitSortType
+import com.dicoding.habitapp.utils.SortUtils
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class HabitRepository(private val habitDao: HabitDao, private val executor: ExecutorService) {
+
+    companion object {
+
+        @Volatile
+        private var instance: HabitRepository? = null
+
+        fun getInstance(context: Context): HabitRepository {
+            return instance ?: synchronized(this) {
+                if (instance == null) {
+                    val database = HabitDatabase.getInstance(context)
+                    instance = HabitRepository(
+                        database.habitDao(),
+                        Executors.newSingleThreadExecutor()
+                    )
+                }
+                return instance as HabitRepository
+            }
+
+        }
+    }
+
+    //TODO 4 : Use SortUtils.getSortedQuery to create sortable query and build paged list
+
+    fun getHabits(filter: HabitSortType): LiveData<PagedList<Habit>> {
+        val filterHabit = SortUtils.getSorteredQuery(filter)
+        val filteredHabit = habitDao.getHabits(filterHabit)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(true)
+            .setInitialLoadSizeHint(PAGE_SIZE)
+            .setPageSize(PAGE_SIZE)
+            .build()
+
+        return LivePagedListBuilder(filteredHabit, config).build()
+    }
+
+    //TODO 5 : Complete other function inside repository
+
+    fun getHabitById(habitId: Int): LiveData<Habit> = habitDao.getHabitById(habitId)
+
+    fun insertHabit(newHabit: Habit) {
+        executor.execute {
+            habitDao.insertHabit(newHabit)
+        }
+    }
+
+    fun deleteHabit(habit: Habit) {
+        executor.execute {
+            habitDao.deleteHabit(habit)
+        }
+    }
+
+    fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit> = habitDao.getRandomHabitByPriorityLevel(level)
+}
